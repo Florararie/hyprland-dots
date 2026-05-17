408095c5a5fbe27e73686684d592df9b351d77fe
Florararie <samueltayloratkinson1@gmail.com>
2026-05-16 23:56:55 -0400

    Forgor

--- .config/hypr/config/autostart.lua
+++ .config/hypr/config/autostart.lua
@@ -3,7 +3,8 @@
 hl.on("hyprland.start", function () 
     hl.exec_cmd("kill kded6")
     hl.exec_cmd("hyprpaper")
-    hl.exec_cmd("wayle panel start")
+    --hl.exec_cmd("wayle panel start")
+    hl.exec_cmd("qs -c noctalia-shell")
     hl.exec_cmd("hypridle -c ~/.config/hypr/hypridle.conf")
     hl.exec_cmd("corectrl --minimize-systray")
     --hl.exec_cmd("~/.config/hypr/scripts/moveOpenedGames.sh") -- disabled by default since this behavior may not be desirable
