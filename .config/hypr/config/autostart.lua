dbd97bac9e0e6b54579062db8d9dd3a6fb211959
Florararie <samueltayloratkinson1@gmail.com>
2026-09-15 16:45:31 -0400

    Update autostart.lua to manage startup commands
    Re-enable wayle panel startup command and comment out alternative options.

--- .config/hypr/config/autostart.lua
+++ .config/hypr/config/autostart.lua
@@ -3,12 +3,14 @@
 hl.on("hyprland.start", function () 
     hl.exec_cmd("kill kded6")
     hl.exec_cmd("hyprpaper")
-    hl.exec_cmd("wayle panel start")
-    --hl.exec_cmd("qs -c noctalia-shell")
     hl.exec_cmd("hypridle -c ~/.config/hypr/hypridle.conf")
-    hl.exec_cmd("corectrl --minimize-systray")
+    --hl.exec_cmd("corectrl --minimize-systray") -- only enable if you plan to tweak
     --hl.exec_cmd("~/.config/hypr/scripts/moveOpenedGames.sh") -- disabled by default since this behavior may not be desirable
 
+    -- Choose only one
+    hl.exec_cmd("wayle panel start")
+    --hl.exec_cmd("qs -c noctalia-shell")
+        
     -- Choose only one
     --hl.exec_cmd("/usr/lib/hyprpolkitagent/hyprpolkitagent")
     hl.exec_cmd("/usr/lib/polkit-kde-authentication-agent-1")
